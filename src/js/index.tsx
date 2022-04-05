2371471f92580c88fb6e524b6d98c742e5d6f752
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2022-04-05 18:07:28 +0900

    Prettierの追加

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "@/js/components/main";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '@/js/components/main';
 
-const container = document.getElementById("app") as HTMLElement;
+const container = document.getElementById('app') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
